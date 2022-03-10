14268b2dbe4ef39d0ed6a80572b0493ce7a2aabf
I-ce Cream <43102378+YoujiaZhang@users.noreply.github.com>
2022-03-10 11:04:11 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <tablebar></tablebar>
+  </div>
+</template>
+
+<script>
+import tablebar from "./components/tableBar.vue";
+export default {
+  name: "App",
+  components:{tablebar},
+};
+</script>
+
+
+<style scoped>
+</style>
